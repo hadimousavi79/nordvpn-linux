afd86da24076b8f7f551d3e9416add17625e3751
Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>
2024-10-30 08:35:28 +0100

    Move handler of norddrop callback off-site

--- cmd/fileshare/main.go
+++ cmd/fileshare/main.go
@@ -135,12 +135,14 @@ func main() {
 		defaultDownloadDirectory = ""
 	}
 
+	ch := make(chan fileshare.Event, 100)
 	eventManager := fileshare.NewEventManager(
 		internal.IsProdEnv(Environment),
 		meshClient,
 		fileshare.StdOsInfo{},
 		fileshare.NewStdFilesystem("/"),
 		defaultDownloadDirectory,
+		ch,
 	)
 
 	privKeyResponse, err := meshClient.GetPrivateKey(context.Background(), &meshpb.Empty{})
@@ -175,6 +177,7 @@ func main() {
 		fileshare.NewPubkeyProvider(meshClient).PubkeyFunc,
 		string(meshPrivKey),
 		storagePath,
+		ch,
 	)
 	if err != nil {
 		log.Println(internal.ErrorPrefix, "can't create fileshare implementation:", err)
